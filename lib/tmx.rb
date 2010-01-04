ccf5c6a666f3f58ff298265962cc4b2a65844562
Eris <eris.discord@gmail.com>
2010-01-04 01:46:25 -0500

    extensible coder module for transcoding map data

--- lib/tmx.rb
+++ lib/tmx.rb
@@ -1,9 +1,12 @@
+require 'base64'
 require 'gosu'
 require 'nokogiri'
+require 'zlib'
 
 require 'tmx/nokogiri_additions'
 
 module TMX
+  autoload :Coder,       'tmx/coder'
   autoload :Layer,       'tmx/layer'
   autoload :Map,         'tmx/map'
   autoload :ObjectGroup, 'tmx/object_group'
